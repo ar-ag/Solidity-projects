b6a7baacb1a3d53520f33cddd25fa64dafaf0bb2
Aryan Agarwal <96348217+ar-ag@users.noreply.github.com>
2022-10-02 18:47:43 +0530

    Piggy Bank in solidity

---
+++ PiggyBank.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+
+contract PiggyBank {
+    address public owner = msg.sender;
+
+    event Deposit(uint amount);
+    event Withdraw(uint amount);
+
+    receive() external payable{
+        emit Deposit(msg.value);
+    }
+    function withdraw() external {
+        require(msg.sender == owner, "not owner");
+        emit Withdraw(address(this).balance);
+        selfdestruct(payable(msg.sender));
+    }
+}
